2c687c9b1c3af7538638ca4230bba3a1d6cdf9b6
christhoval <christhoval@mftech.io>
2023-07-27 00:36:23 -0500

    test mongo

--- lms/domain/generic/registry.py
+++ lms/domain/generic/registry.py
@@ -1,9 +1,11 @@
 """Set of pre-instantiated dependencies for the generic domain."""
+from lms.domain import generic
 from lms.domain.generic.service import GenericService
+from lms.domain.repositories import generic
 
 
 class GenericRegistry:
     """Registry of dependencies for the generic domain."""
 
-    def __init__(self, repository, service=GenericService):
+    def __init__(self, repository: generic.Repository, service=GenericService):
         self.service = service(repository=repository)
--- lms/domain/users/service.py
+++ lms/domain/users/service.py
@@ -39,7 +39,7 @@ class UsersService(GenericService[fields.AdminOut, fields.AdminCreate, fields.Ad
             raise exceptions.DoesExistError(message=f"User with {data_object.email} exists")
 
     async def login(self, username: str, password: str) -> fields.LoginSuccess:
-        print('login self.table', self.table, self.table.email)
+        print('login self.table', self.table, self.table.full_name)
         admin_exists = await self.exist(self.table.email == username)
 
         if not admin_exists:
